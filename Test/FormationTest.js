d726ef31e96d66fb9649ec4d5d49bb4c6a6cfb44
Asmae BENSLIMANE <asmaebenslimane@MacBook-Pro-de-Asmae.local>
2019-06-28 19:50:21 +0200

    Update ClassFormation

--- Test/FormationTest.js
+++ Test/FormationTest.js
@@ -9,5 +9,15 @@ let formation = new Formation({
 
   test('Test validité formation true', () => { expect(formation.testIsValid().is_valid).toBe(true); });
 
+  formation.places = 0;
+
+  test('Test validité formation false car  la place est égale à 0', () => {
+    formation.places = 12  
+    
+    expect(user.testIsValid().is_valid).toBe(false);
+  });
+  
+
+
 
 
