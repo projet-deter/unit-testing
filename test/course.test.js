e98421f5604a914399e8394631ae86b56974715b
Maxime Amore <maxime.marco.amore@gmail.com>
2019-06-28 20:25:15 +0200

    qsfsqdfqs

---
+++ test/course.test.js
@@ -0,0 +1,11 @@
+/*const Course = require ('../Models/Course')
+
+let course = new Course({
+    room: "qsdfqsdfsq",
+    teacher : "Math",
+    interns : 133,
+
+  })
+
+  test('Test validité formation true', () => { expect(course.testIsValid()).toBe(true); });
+*/
